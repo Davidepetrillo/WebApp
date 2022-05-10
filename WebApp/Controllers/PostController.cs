7c105f36658eafa67db3006823f597c2cf5e4705
davidepetrillo <davide96petrillo@gmail.com>
2022-05-10 12:28:36 +0200

    Add project files.

---
+++ WebApp/Controllers/PostController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCore_01.Models;
+using NetCore_01.Utils;
+
+namespace NetCore_01.Controllers
+{
+    public class PostController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+
+            List<Post> posts = PostData.GetPosts();
+
+            return View("HomePage", posts);
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            Post postFound = null;
+
+            foreach (Post post in PostData.GetPosts())
+            {
+                if (post.Id == id)
+                {
+                    postFound = post;
+                    break;
+                }
+            }
+
+            if (postFound != null)
+            {
+                return View("Details", postFound);
+            }
+            else
+            {
+                return NotFound("Il post con id " + id + " non è stato trovato");
+            }
+        }
+    }
+}
---
+++ WebApp/Utils/PostData.cs
@@ -0,0 +1,32 @@
+﻿using NetCore_01.Models;
+
+namespace NetCore_01.Utils
+{
+    public static class PostData
+    {
+
+        private static List<Post> posts;
+
+        public static List<Post> GetPosts()
+        {
+            if (PostData.posts != null)
+            {
+                return PostData.posts;
+            }
+
+            List<Post> nuovaListaPosts = new List<Post>();
+
+            for (int i = 0; i < 0; i++)
+            {
+                Post post = new Post(i, "Titolo post: " + i, "Lorem Ipsum is simply dummy text of the printing and typesetting... ", "https://picsum.photos/id/" + i + "/300/200");
+                nuovaListaPosts.Add(post);
+            }
+
+            PostData.posts = nuovaListaPosts;
+
+            return PostData.posts;
+
+        }
+
+    }
+}
